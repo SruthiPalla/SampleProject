9460a17e2aeb65611fecdfd5e96eb2d977c6bcd2
sruthi <sruthidady46@gmail.com>
2022-10-16 22:13:17 +0530

    Changes done by dev2 first time in gitDemo1.java

--- gitDemo1.java
+++ gitDemo1.java
@@ -2,6 +2,6 @@ class gitDemo1
 {
  public static void main(String args[])
 	{
-		System.out.println("This is git demo program");
+		System.out.println("This is git demo program changed by second developer after cloning for first time");
 	}
 }
