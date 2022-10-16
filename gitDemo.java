f5031a63321ebe719cd64df42885a6a6d30f7926
sruthi <sruthidady46@gmail.com>
2022-10-16 22:06:36 +0530

    Changes done by dev1 for the first time

--- gitDemo.java
+++ gitDemo.java
@@ -2,6 +2,6 @@ class gitDemo
 {
  public static void main(String args[])
 	{
-		System.out.println("This is git demo program");
+		System.out.println("This is git demo program changed first time by developer1");
 	}
 }
