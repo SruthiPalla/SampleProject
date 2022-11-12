7cedec82aac21903a9c1a04f38dade5dc55dacc4
sruthi <sruthidady46@gmail.com>
2022-11-13 02:28:14 +0530

    clone sample project, changed gitDemo.java, push

--- gitDemo.java
+++ gitDemo.java
@@ -3,5 +3,8 @@ class gitDemo
  public static void main(String args[])
 	{
 		System.out.println("This is git demo program changed first time by developer1");
+		
+		System.out.println("changes done after closning this project and then changed and then push");
+
 	}
 }
